23f4f51883f18cd4b1f126b39f3acacbf2d96697
Hineill <hineill.cespedes@pucp.edu.pe>
2024-05-09 19:37:51 -0500

    Cuchao

--- src/main/java/com/example/lab6gtics/Controller/HomeController.java
+++ src/main/java/com/example/lab6gtics/Controller/HomeController.java
@@ -1,11 +1,20 @@
 package com.example.lab6gtics.Controller;
 
+import com.example.lab6gtics.DTO.EquiposPorPais;
 import com.example.lab6gtics.Entity.Technician;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import com.example.lab6gtics.Repository.*;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -38,6 +47,44 @@ public class HomeController {
     @GetMapping("/createTech")
     public String CreateTechnician(Model model , @ModelAttribute("technician") Technician tech){
         return "EditCreateTechnician";
+    }
+    @PostMapping("/createTechCompleted")
+    public String CreateTechnicianCompleted(Model model, @ModelAttribute("technician") @Valid Technician technician ,
+                                            BindingResult bindingResult, RedirectAttributes attr){
+        if(!bindingResult.hasErrors()) {
+            if (!(technician.getTechnicianId() == 0)) {
+                attr.addFlashAttribute("msg", "Tecnico" + technician.getFirstName() + " " + technician.getLastName() + "fue actualizado   exitosamente ");
+                technicianRepository.save(technician);
+
+            } else {
+                Technician tech = technicianRepository.save(technician);
+                attr.addFlashAttribute("msg", "Tecnico" + tech.getFirstName() + " " + tech.getLastName() + "fue creado exitosamente ");
+            }
+            technicianRepository.save(technician);
+            return "redirect:/TecnicosList";
+        }else{
+            System.out.println("HUBO ERRORES D:");
+            return "EditCreateTechnician";
+        }
+    }
+
+    @GetMapping("/EditTech")
+    public String EditTechnician(Model model , @ModelAttribute("technician") Technician technician , @RequestParam("id") int idTech){
+        Optional<Technician> techAux = technicianRepository.findById(idTech);
+        if(techAux.isPresent()){
+            technician = techAux.get();
+            model.addAttribute("technician", technician);
+            System.out.println(technician.getFirstName());
+        }
+        return "EditCreateTechnician";
+    }
+
+    @GetMapping("/EstadisticasList")
+    public String EstadisticasList(Model model) {
+        System.out.println("EstadisticasList");
+        List<EquiposPorPais> lista = deviceRepository.getEquiposPorPaisDTO();
+        model.addAttribute("lista" , lista);
+        return "Estadisticas";
     }
 
 
